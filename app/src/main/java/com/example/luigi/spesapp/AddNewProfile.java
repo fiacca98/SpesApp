261c9ddd915fbab2a2b1997887bfc03d6b58f357
SimoneBergomi <bergomi.simone@gmail.com>
2018-04-18 10:40:39 +0200

    menu

---
+++ app/src/main/java/com/example/luigi/spesapp/AddNewProfile.java
@@ -0,0 +1,16 @@
+package com.example.luigi.spesapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by simone on 18/04/2018.
+ */
+public class AddNewProfile extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina_profilo);
+    }
+
+}
--- app/src/main/java/com/example/luigi/spesapp/MainActivity.java
+++ app/src/main/java/com/example/luigi/spesapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -15,11 +15,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -94,6 +94,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
+            case R.id.profile:
+                Intent intent = new Intent(this, AddNewProfile.class);
+
+                this.startActivity(intent);
+                break;
             case R.id.action_change: {
 
                 switch (mCurrentLayoutManagerType) {
@@ -108,12 +113,13 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
-                return true;
+
             }
             default: {
                 return super.onOptionsItemSelected(item);
             }
         }
+        return true;
     }
 
 
